d1e67b536aed339b850898ac6d6e77984544ed71
Daniel <150309766+dgyoungk@users.noreply.github.com>
2023-12-15 03:38:01 -0800

    Add method outlines for displaying game text

--- Displayable.rb
+++ Displayable.rb
@@ -1,10 +1,19 @@
 module Displayable
 
-  def display_board(board)
-    puts %( #{board[0]} | #{board[1]} | #{board[2]} \n) +
+  def display_board(area)
+    puts %( #{area[0]} | #{area[1]} | #{area[2]} \n) +
          %(---|---|---\n) +
-         %( #{board[3]} | #{board[4]} | #{board[5]} \n) +
+         %( #{area[3]} | #{area[4]} | #{area[5]} \n) +
          %(---|---|---\n) +
-         %( #{board[6]} | #{board[7]} | #{board[8]} \n)
+         %( #{area[6]} | #{area[7]} | #{area[8]} \n)
+  end
+
+  def announce_winner(player)
+  end
+
+  def declare_draw()
+  end
+
+  def alert_turn(player)
   end
 end

31f1f3f89396c0dffcf88c4e6e2ce972efc08ff0
Daniel <150309766+dgyoungk@users.noreply.github.com>
2023-12-15 03:38:59 -0800

    Refactor logic for game_over method
    Changed the logic for determing when the game is over.
    Also added some logic for turn-based play.

--- Game.rb
+++ Game.rb
@@ -3,12 +3,7 @@ require './Displayable.rb'
 class Game
   include Displayable
 
-  # I need a flag to indicate whether a game is over, whether win draw or lose
-  # win condition for either players: marker is in 3 consecutive slots (verti, horiz, diag)
-  # so either 0-2, 3-5, 6-8 (horiz), 0 3 6, 1 4 7, 2 5 8 (verti), or 0 4 8, 2 4 6 (diag)
-
   WIN_CONS = [(0..2).to_a, (3..5).to_a, (6..8).to_a, [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-
 
   attr_accessor :board, :p1, :p2, :occupied
 
@@ -17,6 +12,11 @@ class Game
     self.p1 = p1
     self.p2 = p2
     self.occupied = []
+    @turns = 0
+  end
+
+  def show
+    display_board(self.board.game_area)
   end
 
   # checks whether a board position is already occupied before placing marker
@@ -24,6 +24,7 @@ class Game
     if position > 0 && position < 10
       if self.occupied.empty? || !self.occupied.include?(position - 1)
         self.board.update_board(player, position)
+        update_turns()
         self.occupied << position - 1
       else
         puts "That position is taken, try again \n\n"
@@ -35,11 +36,36 @@ class Game
 
   # checks whether a player has won or all slots are filled up
   def game_over?(p1, p2)
-    if self.occupied.length == 8
+    p1_win = WIN_CONS.any? { |combo| combo.intersection(p1.marked) == combo }
+    p2_win = WIN_CONS.any? { |combo| combo.intersection(p2.marked) == combo }
+
+    if p1_win
+      announce_winner(p1)
+      p1_win
+    elsif p2_win
+      announce_winner(p2)
+      p2_win
+    elsif self.occupied.length == 8
+      declare_draw()
       true
     else
-      return WIN_CONS.any? { |combo| combo.intersection(p1.marked) == combo }
+      false
     end
-    false
+  end
+
+  private
+
+  def update_turns
+    @turns += 1
+  end
+
+  attr_reader :turns
+
+  def determine_turn
+    if self.turns.even?
+      alert_turn(p1)
+    elsif self.turns.odd?
+      alert_turn(p2)
+    end
   end
 end

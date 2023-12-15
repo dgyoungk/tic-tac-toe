dfe26a6d64359abbfee0867c54a596e6f09ff8e3
Daniel <150309766+dgyoungk@users.noreply.github.com>
2023-12-15 03:37:17 -0800

    Change name of board array to area for clarity

--- Board.rb
+++ Board.rb
@@ -1,15 +1,15 @@
 class Board
-  attr_accessor :board
+  attr_accessor :game_area
 
   def initialize
-    self.board = Array.new(8)
-    for i in 0..board.length
-      self.board[i] = " "
+    self.game_area = Array.new(8)
+    for i in 0..game_area.length
+      self.game_area[i] = " "
     end
   end
 
   def update_board(player, position)
-    self.board[position - 1] = player.marker
+    self.game_area[position - 1] = player.marker
     player.update_positions(position - 1)
   end
 end
